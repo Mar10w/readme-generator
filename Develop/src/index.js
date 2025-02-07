4740ebfc3b030ba039ed19f30463912636f45599
D M <marlowthepirate@hotmail.com>
2025-02-07 15:41:21 -0600

    added packages, writeto, and func to init.

---
+++ Develop/src/index.js
@@ -0,0 +1,32 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './generateMarkdown.js';
+import { questions } from './utils/questions.js';
+
+// TODO: Create an array of questions for user input
+//const questions = [];
+
+// TODO: Create a function to write README file
+//function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data, (err) =>
+      err ? console.error(err) : console.log('README.md has been generated!')
+    );
+  }
+  
+
+// TODO: Create a function to initialize app
+//function init() {}
+async function init() {
+    try {
+      const userInput = await inquirer.prompt(questions);
+      const markdownContent = generateMarkdown(userInput);
+      writeToFile('README.md', markdownContent);
+    } catch (error) {
+      console.error('Error initializing the application:', error);
+    }
+  }
+
+// Function call to initialize app
+init();
